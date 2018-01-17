cffba8da5097a59c861cbe9bbf57289582858954
Sam Mamane <sam.mamane@hotmail.fr>
2018-01-17 14:20:16 +0200

    Stop to open window on mac

--- demo/app.js
+++ demo/app.js
@@ -8,13 +8,14 @@ const mainWindowState = new WindowStateManager('mainWindow', {
     defaultWidth: 1024,
     defaultHeight: 768
 });
-
+let willQuitApp = false;
+let mainWindow;
 app.on('ready', () => {
     // When creating a new BrowserWindow
     // you can assign the properties of the mainWindowState.
     // If a window with the name 'main' was saved before,
     // the saved values will now be assigned to the BrowserWindow again
-    const mainWindow = new BrowserWindow({titleBarStyle: 'hidden',
+    mainWindow = new BrowserWindow({titleBarStyle: 'hidden',
         width: mainWindowState.width,
         height: mainWindowState.height,
         x: mainWindowState.x,
@@ -30,34 +31,18 @@ app.on('ready', () => {
 
     // Don't forget to save the current state
     // of the Browser window when it's about to be closed
-    mainWindow.on('close', () => {
-        mainWindowState.saveState(mainWindow);
+    mainWindow.on('close', (e) => {
+        if (willQuitApp) {
+            /* the user tried to quit the app */
+            mainWindow = null;
+        } else {
+            /* the user only tried to close the window */
+            e.preventDefault();
+            mainWindow.hide();
+        }
     });
 
 });
-app.on('window-all-closed', () => {
-if (process.platform !== 'darwin') {
-  app.quit()
-  }
-})
-app.on('activate', () => {
-  const mainWindow = new BrowserWindow({titleBarStyle: 'hidden',
-      width: mainWindowState.width,
-      height: mainWindowState.height,
-      x: mainWindowState.x,
-      y: mainWindowState.y
-  });
-  mainWindow.loadURL('file://' + __dirname + '/electron-tabs.html');
+app.on('activate', () => mainWindow.show());
 
-  // You can check if the window was closed in a maximized saveState
-  // If so you can maximize the BrowserWindow again
-  if (mainWindowState.maximized) {
-      mainWindow.maximize();
-  }
-
-  // Don't forget to save the current state
-  // of the Browser window when it's about to be closed
-  mainWindow.on('close', () => {
-      mainWindowState.saveState(mainWindow);
-  });
-})
+app.on('before-quit', () => willQuitApp = true);
